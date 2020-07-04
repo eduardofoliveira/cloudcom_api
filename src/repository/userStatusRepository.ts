c04204768886de8ec33b31489b831db5c3f36f18
Eduardo Felipe de Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2020-07-04 02:18:59 -0300

    Alteração de query UserStatusRepository

--- src/repository/userStatusRepository.ts
+++ src/repository/userStatusRepository.ts
@@ -20,9 +20,47 @@ class UserStatusRepository {
   public async getStatusByDomain({
     domain,
   }: MethodGetStatusByDomain): Promise<UserStatus[] | undefined> {
+    // const userStatusList = await connectionOracle.raw<RetornoDbStatus[]>(`
+    //   select
+    //       u.vch_username as usuario,
+    //       u2.vch_username as usuario_terminal,
+    //       ssl.vch_useragent as useragent,
+    //       d.vch_domain as dominio,
+    //       ac.vch_originalto as destino,
+    //       replace(replace(ac.int_calltype, 6, 'Discada'), 7, 'Recebida') as tipo,
+    //       replace(replace(replace(replace(replace(ac.int_state, 3, 'Conectado'), 2, 'Chamando'), 1, 'Tentando'), 0, 'Conexão'), 4, 'Espera') as estado
+    //   from
+    //       tbl_sys_user u,
+    //       tbl_sys_domain d,
+    //       tbl_pbx_pbxuser pu
+    //   FULL OUTER JOIN tbl_pbx_terminal t
+    //       ON t.int_pbxuser_key = pu.int_pbxuser_key
+    //   FULL OUTER JOIN tbl_pbx_pbxuserterminal put
+    //       ON put.int_terminal_key = t.int_terminal_key
+    //   FULL OUTER JOIN tbl_pbx_pbxuser pu2
+    //       ON pu2.int_pbxuser_key = put.int_pbxuser_key
+    //   FULL OUTER JOIN tbl_sys_user u2
+    //       ON u2.int_user_key = pu2.int_user_key
+    //   FULL OUTER JOIN tbl_pbx_address a
+    //       ON a.int_pbxuser_key = pu.int_pbxuser_key and a.int_type = 2
+    //   FULL OUTER JOIN tbl_pbx_activecall ac
+    //       ON ac.int_address_key = a.int_address_key,
+    //       tbl_sys_sessionlog sl,
+    //       tbl_sys_sipsessionlog ssl
+    //   where
+    //       u.int_user_key = pu.int_user_key and
+    //       u.int_domain_key = d.int_domain_key and
+    //       sl.int_user_key = u.int_user_key and
+    //       ssl.int_sessionlog_key = sl.int_sessionlog_key and
+    //       u.int_active = 1 and
+    //       u.int_agentuser in (0, 7) and
+    //       d.vch_domain = '${domain}'
+    //   order by
+    //       u.vch_name
+    // `);
     const userStatusList = await connectionOracle.raw<RetornoDbStatus[]>(`
       select
-          u.vch_username as usuario,
+          unique u.vch_username as usuario,
           u2.vch_username as usuario_terminal,
           ssl.vch_useragent as useragent,
           d.vch_domain as dominio,
@@ -30,33 +68,36 @@ class UserStatusRepository {
           replace(replace(ac.int_calltype, 6, 'Discada'), 7, 'Recebida') as tipo,
           replace(replace(replace(replace(replace(ac.int_state, 3, 'Conectado'), 2, 'Chamando'), 1, 'Tentando'), 0, 'Conexão'), 4, 'Espera') as estado
       from
-          tbl_sys_user u,
           tbl_sys_domain d,
-          tbl_pbx_pbxuser pu
+          tbl_sys_user u
+      FULL OUTER JOIN tbl_pbx_pbxuser pu
+          ON pu.int_user_key = u.int_user_key
+      FULL OUTER JOIN tbl_pbx_address a
+          ON a.int_pbxuser_key = pu.int_pbxuser_key
       FULL OUTER JOIN tbl_pbx_terminal t
-          ON t.int_pbxuser_key = pu.int_pbxuser_key
+        ON t.int_pbxuser_key = pu.int_pbxuser_key
       FULL OUTER JOIN tbl_pbx_pbxuserterminal put
-          ON put.int_terminal_key = t.int_terminal_key
+        ON put.int_terminal_key = t.int_terminal_key
       FULL OUTER JOIN tbl_pbx_pbxuser pu2
-          ON pu2.int_pbxuser_key = put.int_pbxuser_key
+        ON pu2.int_pbxuser_key = put.int_pbxuser_key
       FULL OUTER JOIN tbl_sys_user u2
           ON u2.int_user_key = pu2.int_user_key
-      FULL OUTER JOIN tbl_pbx_address a
-          ON a.int_pbxuser_key = pu.int_pbxuser_key and a.int_type = 2
+      FULL OUTER JOIN tbl_pbx_address a2
+          ON a2.int_pbxuser_key = pu2.int_pbxuser_key
       FULL OUTER JOIN tbl_pbx_activecall ac
-          ON ac.int_address_key = a.int_address_key,
-          tbl_sys_sessionlog sl,
-          tbl_sys_sipsessionlog ssl
+          ON ac.int_address_key in (a.int_address_key, a2.int_address_key )
+      FULL OUTER JOIN tbl_sys_sessionlog sl
+          ON sl.int_user_key = u.int_user_key
+      FULL OUTER JOIN tbl_sys_sipsessionlog ssl
+          ON ssl.int_sessionlog_key = sl.int_sessionlog_key
       where
-          u.int_user_key = pu.int_user_key and
-          u.int_domain_key = d.int_domain_key and
-          sl.int_user_key = u.int_user_key and
-          ssl.int_sessionlog_key = sl.int_sessionlog_key and
-          u.int_active = 1 and
+          d.vch_domain = '${domain}' and
+          d.int_domain_key = u.int_domain_key and
           u.int_agentuser in (0, 7) and
-          d.vch_domain = '${domain}'
+          u.int_active = 1 and
+          ssl.vch_useragent is not null
       order by
-          u.vch_name
+          u.vch_username
     `);
 
     if (userStatusList.length > 0) {
